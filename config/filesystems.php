2866cb4f549bcb7765a0d3fbae09193dc773ded1
Yasir Osman <yasirosman13@gmail.com>
2020-04-27 03:21:20 +0100

    cloud storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,9 +60,7 @@ return [
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
+            'bucket' => env('AWS_BUCKET')
         ],
 
     ],
